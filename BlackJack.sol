df8dbd0f66549bcfc328156634c9831e44808665
Viktor Urban <v.gsteiger@gmail.com>
2019-10-22 14:05:06 +0200

    Created very first version of our smart Contract

---
+++ BlackJack.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.4.24;
+
+//  Blackjack smart contract to be deployed on Rinkeby testnet
+//  Contributors:
+//    Tim Keller and Viktor Gsteiger
+
+// Current version: 22.10.2019
+
+contract BlackJack {
+
+}
