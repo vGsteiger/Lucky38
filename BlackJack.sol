fe245d30657b9aa78cfd85fbd53dd474c574252d
Viktor Urban <v.gsteiger@gmail.com>
2019-10-22 14:12:09 +0200

    Created basic structure and TODOs

--- BlackJack.sol
+++ BlackJack.sol
@@ -5,7 +5,14 @@ pragma solidity ^0.4.24;
 //    Tim Keller and Viktor Gsteiger
 
 // Current version: 22.10.2019
+// TODOs for V1 due 04.11.2019
 
 contract BlackJack {
+  //state variables
 
+  // Modifiers
+  //TODO: isValidAddress, isPlayer, playerTurn, newRound
+
+  // Functions
+  //TODO: randomNumber, placeBet, cashOut, deal, hit, stand, showTable
 }
