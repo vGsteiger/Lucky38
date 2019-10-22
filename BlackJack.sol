142fcaa54e1eb1896e49906bb5b09295c3fd1e75
Viktor Urban <v.gsteiger@gmail.com>
2019-10-22 17:56:10 +0200

    Implemented our needed modifiers and some structuring

--- BlackJack.sol
+++ BlackJack.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.24;
+pragma solidity ^0.4.25;
 
 //  Blackjack smart contract to be deployed on Rinkeby testnet
 //  Contributors:
@@ -8,10 +8,48 @@ pragma solidity ^0.4.24;
 // TODOs for V1 due 04.11.2019
 
 contract BlackJack {
-  //state variables
+
+  // DECLARATION START
+
+  address private _playerAddress;
+
+  // TODO: Ist noch nicht ausgefleischt, wäre aber eine schöne
+  // Version, die Karten zu speichern
+  struct Hand {
+
+  }
+
+  bool private _turn;
+
+  uint256 private _currentBalance;
+  uint256 private _randomNumber;
+
+  // DECLARATION END
+
+  // ----------
+
+  // MODIFIERS START
 
   // Modifiers
-  //TODO: isValidAddress, isPlayer, playerTurn, newRound
+  // Control whether Address is in fact player
+  modifier isPlayer() {
+    require(msg.sender == _playerAddress);
+    _;
+  }
+
+  // To be able to only use a function during a game
+  modifier inRound() {
+    require(_turn == true, "No game running.");
+    _;
+  }
+
+  // To be able to only use a function after or before a game
+  modifier outRound() {
+    require(_turn == false, "Game running");
+    _;
+  }
+
+  // MODIFIERS END
 
   // Functions
   //TODO: randomNumber, placeBet, cashOut, deal, hit, stand, showTable
